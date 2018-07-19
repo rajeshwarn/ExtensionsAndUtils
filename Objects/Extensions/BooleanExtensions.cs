739a703974c106450fb9039b2781162f7530b199
Ninita1 <Ninita1@users.noreply.github.com>
2018-07-19 15:29:30 +0100

    Add files via upload

---
+++ Objects/Extensions/BooleanExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Shared.Extensions
+{
+    public static class BooleanExtensions
+    {
+        public static bool GetNullableBoolValue(this bool? value)
+        {
+            if (value == null)
+                return true;
+            return bool.Parse(value.ToString());
+        }
+
+        public static string ToIntAsString(this bool value)
+        {
+            return IntAsString(value);
+        }
+
+        public static string ToIntAsString(this bool? value)
+        {
+            return IntAsString(value);
+        }
+
+        private static string IntAsString(object value)
+        {
+            return value != null ? Convert.ToInt32(value).ToString(CultureInfo.InvariantCulture) : null;
+        }
+    }
+}
