9eecd38bd6a879b8ade984aec1c59776185facf2
Ninita1 <Ninita1@users.noreply.github.com>
2018-08-28 09:55:47 +0100

    Delete WelcomeCommunication.js

--- ASP.Net/Frontend/Communication/js/Modules/WelcomeCommunication.js
+++
@@ -1,7 +0,0 @@
-﻿var MyModuleCommunication = new function () {
-
-    this.Call_GetSomething = function (parameters, succeededHandler, errorHandler) {
-        CallAjaxServiceAsync("MyModule_GetSomething", parameters, succeededHandler, errorHandler);
-    }
-    
-}
